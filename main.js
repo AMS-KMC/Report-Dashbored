27ad56488bf98e27ae2f9a182e305ec6ea29b4b3
shayanTaki <takishayan@gmail.com>
2023-08-08 09:07:31 -0700

    end making Hover Style

--- main.js
+++ main.js
@@ -41,6 +41,48 @@ function resetStyleHome() {
 
 
 
+
+
+// function changeStyleHome() {
+//   let element2 = document.querySelector(".flex-row .svg-base");
+//   element2.classList.remove("svg-base");
+//   element2.classList.add("svg-base-off");
+
+
+//   let element = document.querySelector(".flex-row .svg-on-hover-off");
+//   element.classList.remove("svg-on-hover-off");
+//   element.classList.add("svg-on-hover");
+
+
+//   let element3 =  document.querySelector(".flex-row .mtn-base");
+//   element3.classList.remove("mtn-base");
+//   element3.classList.add("mtn-on-hover");
+
+
+//   let element4 =  document.querySelector(".flex-row");
+//   element4.classList.remove("flex-base");
+//   element4.classList.add("flex-on-hover");
+// }
+// function resetStyleHome() {
+//   let element2 = document.querySelector(".flex-row .svg-base-off");
+//   element2.classList.remove("svg-base-off");
+//   element2.classList.add("svg-base");
+
+//   let element = document.querySelector(".flex-row .svg-on-hover");
+//   element.classList.remove("svg-on-hover");
+//   element.classList.add("svg-on-hover-off");
+
+//   let element3 =  document.querySelector(".flex-row .mtn-on-hover");
+//   element3.classList.remove("mtn-on-hover");
+//   element3.classList.add("mtn-base");
+
+//   let element4 =  document.querySelector(".flex-row");
+//   element4.classList.remove("flex-on-hover");
+//   element4.classList.add("flex-base");
+// }
+
+
+
 // @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
   
 // Auth Function
