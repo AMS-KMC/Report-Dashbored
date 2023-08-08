deb688b840bd655df6b4d5dce6500c778af13ebf
shayanTaki <takishayan@gmail.com>
2023-08-08 05:58:05 -0700

    making style 4

--- main.js
+++ main.js
@@ -1,13 +1,17 @@
-// JavaScript
+JavaScript
 function changeStyle() {
-  let element = document.querySelector(".svg-Auth-off");
-  element.classList.remove("svg-Auth-off");
-  element.classList.add("svg-Auth-on");
+  let element2 = document.querySelector(".svg-base-home");
+  element2.classList.remove("svg-base-home");
+  element2.classList.add("home-base-off");
+  let element = document.querySelector(".home-off");
+  element.classList.remove("home-off");
+  element.classList.add("svg-home-on-hover");
 }
 
   
-function resetStyle() {
-  let element = document.querySelector(".svg-Auth-on");
-  element.classList.remove("svg-Auth-on");
-  element.classList.add("svg-Auth-off");
-}
+// function resetStyle() {
+//   let element = document.querySelector(".svg-Auth-on");
+//   element.classList.remove("svg-Auth-on", "svg-base");
+//   element.classList.add("svg-Auth-off","svg-base-off");
+// }
+
