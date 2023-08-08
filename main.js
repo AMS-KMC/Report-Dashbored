59b597244fd32d4109baf76317bcdc2c42ee1689
shayanTaki <takishayan@gmail.com>
2023-08-08 09:01:25 -0700

    end making Hover Style

--- main.js
+++ main.js
@@ -46,45 +46,252 @@ function resetStyleHome() {
 // Auth Function
 
 
-function changeStyleAuth() {
-  let element2 = document.querySelector(".flex-row-Auth .svg-base");
+function changeStyleAuth1() {
+  let element2 = document.querySelector(".flex-row-Auth1 .svg-base");
   element2.classList.remove("svg-base");
   element2.classList.add("svg-base-off");
 
 
-  let element = document.querySelector(".flex-row-Auth .svg-on-hover-off");
+  let element = document.querySelector(".flex-row-Auth1 .svg-on-hover-off");
   element.classList.remove("svg-on-hover-off");
   element.classList.add("svg-on-hover");
 
 
-  let element3 =  document.querySelector(".flex-row-Auth .mtn-base");
+  let element3 =  document.querySelector(".flex-row-Auth1 .mtn-base");
   element3.classList.remove("mtn-base");
   element3.classList.add("mtn-on-hover");
 
 
-  let element4 =  document.querySelector(".flex-row-Auth");
+  let element4 =  document.querySelector(".flex-row-Auth1");
   element4.classList.remove("flex-base");
   element4.classList.add("flex-on-hover");
 }
-function resetStyleAuth() {
-  let element2 = document.querySelector(".flex-row-Auth .svg-base-off");
+function resetStyleAuth1() {
+  let element2 = document.querySelector(".flex-row-Auth1 .svg-base-off");
   element2.classList.remove("svg-base-off");
   element2.classList.add("svg-base");
 
-  let element = document.querySelector(".flex-row-Auth .svg-on-hover");
+  let element = document.querySelector(".flex-row-Auth1 .svg-on-hover");
   element.classList.remove("svg-on-hover");
   element.classList.add("svg-on-hover-off");
 
-  let element3 =  document.querySelector(".flex-row-Auth .mtn-on-hover");
+  let element3 =  document.querySelector(".flex-row-Auth1 .mtn-on-hover");
   element3.classList.remove("mtn-on-hover");
   element3.classList.add("mtn-base");
 
-  let element4 =  document.querySelector(".flex-row-Auth");
+  let element4 =  document.querySelector(".flex-row-Auth1");
   element4.classList.remove("flex-on-hover");
   element4.classList.add("flex-base");
 }
 
+// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%Auth2%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+
+
+function changeStyleAuth2() {
+  let element2 = document.querySelector(".flex-row-Auth2 .svg-base");
+  element2.classList.remove("svg-base");
+  element2.classList.add("svg-base-off");
+
+
+  let element = document.querySelector(".flex-row-Auth2 .svg-on-hover-off");
+  element.classList.remove("svg-on-hover-off");
+  element.classList.add("svg-on-hover");
+
+
+  let element3 =  document.querySelector(".flex-row-Auth2 .mtn-base");
+  element3.classList.remove("mtn-base");
+  element3.classList.add("mtn-on-hover");
+
+
+  let element4 =  document.querySelector(".flex-row-Auth2");
+  element4.classList.remove("flex-base");
+  element4.classList.add("flex-on-hover");
+}
+function resetStyleAuth2() {
+  let element2 = document.querySelector(".flex-row-Auth2 .svg-base-off");
+  element2.classList.remove("svg-base-off");
+  element2.classList.add("svg-base");
+
+  let element = document.querySelector(".flex-row-Auth2 .svg-on-hover");
+  element.classList.remove("svg-on-hover");
+  element.classList.add("svg-on-hover-off");
+
+  let element3 =  document.querySelector(".flex-row-Auth2 .mtn-on-hover");
+  element3.classList.remove("mtn-on-hover");
+  element3.classList.add("mtn-base");
+
+  let element4 =  document.querySelector(".flex-row-Auth2");
+  element4.classList.remove("flex-on-hover");
+  element4.classList.add("flex-base");
+}
+
+// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%Auth3%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+
+function changeStyleAuth3() {
+  let element2 = document.querySelector(".flex-row-Auth3 .svg-base");
+  element2.classList.remove("svg-base");
+  element2.classList.add("svg-base-off");
+
+
+  let element = document.querySelector(".flex-row-Auth3 .svg-on-hover-off");
+  element.classList.remove("svg-on-hover-off");
+  element.classList.add("svg-on-hover");
+
+
+  let element3 =  document.querySelector(".flex-row-Auth3 .mtn-base");
+  element3.classList.remove("mtn-base");
+  element3.classList.add("mtn-on-hover");
+
+
+  let element4 =  document.querySelector(".flex-row-Auth3");
+  element4.classList.remove("flex-base");
+  element4.classList.add("flex-on-hover");
+}
+function resetStyleAuth3() {
+  let element2 = document.querySelector(".flex-row-Auth3 .svg-base-off");
+  element2.classList.remove("svg-base-off");
+  element2.classList.add("svg-base");
+
+  let element = document.querySelector(".flex-row-Auth3 .svg-on-hover");
+  element.classList.remove("svg-on-hover");
+  element.classList.add("svg-on-hover-off");
+
+  let element3 =  document.querySelector(".flex-row-Auth3 .mtn-on-hover");
+  element3.classList.remove("mtn-on-hover");
+  element3.classList.add("mtn-base");
+
+  let element4 =  document.querySelector(".flex-row-Auth3");
+  element4.classList.remove("flex-on-hover");
+  element4.classList.add("flex-base");
+}
+
+// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%Auth4%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+
+function changeStyleAuth4() {
+  let element2 = document.querySelector(".flex-row-Auth4 .svg-base");
+  element2.classList.remove("svg-base");
+  element2.classList.add("svg-base-off");
+
+
+  let element = document.querySelector(".flex-row-Auth4 .svg-on-hover-off");
+  element.classList.remove("svg-on-hover-off");
+  element.classList.add("svg-on-hover");
+
+
+  let element3 =  document.querySelector(".flex-row-Auth4 .mtn-base");
+  element3.classList.remove("mtn-base");
+  element3.classList.add("mtn-on-hover");
+
+
+  let element4 =  document.querySelector(".flex-row-Auth4");
+  element4.classList.remove("flex-base");
+  element4.classList.add("flex-on-hover");
+}
+function resetStyleAuth4() {
+  let element2 = document.querySelector(".flex-row-Auth4 .svg-base-off");
+  element2.classList.remove("svg-base-off");
+  element2.classList.add("svg-base");
+
+  let element = document.querySelector(".flex-row-Auth4 .svg-on-hover");
+  element.classList.remove("svg-on-hover");
+  element.classList.add("svg-on-hover-off");
+
+  let element3 =  document.querySelector(".flex-row-Auth4 .mtn-on-hover");
+  element3.classList.remove("mtn-on-hover");
+  element3.classList.add("mtn-base");
+
+  let element4 =  document.querySelector(".flex-row-Auth4");
+  element4.classList.remove("flex-on-hover");
+  element4.classList.add("flex-base");
+}
+
+// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%Auth5%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+
+function changeStyleAuth5() {
+  let element2 = document.querySelector(".flex-row-Auth5 .svg-base");
+  element2.classList.remove("svg-base");
+  element2.classList.add("svg-base-off");
+
+
+  let element = document.querySelector(".flex-row-Auth5 .svg-on-hover-off");
+  element.classList.remove("svg-on-hover-off");
+  element.classList.add("svg-on-hover");
+
+
+  let element3 =  document.querySelector(".flex-row-Auth5 .mtn-base");
+  element3.classList.remove("mtn-base");
+  element3.classList.add("mtn-on-hover");
+
+
+  let element4 =  document.querySelector(".flex-row-Auth5");
+  element4.classList.remove("flex-base");
+  element4.classList.add("flex-on-hover");
+}
+function resetStyleAuth5() {
+  let element2 = document.querySelector(".flex-row-Auth5 .svg-base-off");
+  element2.classList.remove("svg-base-off");
+  element2.classList.add("svg-base");
+
+  let element = document.querySelector(".flex-row-Auth5 .svg-on-hover");
+  element.classList.remove("svg-on-hover");
+  element.classList.add("svg-on-hover-off");
+
+  let element3 =  document.querySelector(".flex-row-Auth5 .mtn-on-hover");
+  element3.classList.remove("mtn-on-hover");
+  element3.classList.add("mtn-base");
+
+  let element4 =  document.querySelector(".flex-row-Auth5");
+  element4.classList.remove("flex-on-hover");
+  element4.classList.add("flex-base");
+}
+
+// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%Auth6%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+function changeStyleAuth6() {
+  let element2 = document.querySelector(".flex-row-Auth6 .svg-base");
+  element2.classList.remove("svg-base");
+  element2.classList.add("svg-base-off");
+
+
+  let element = document.querySelector(".flex-row-Auth6 .svg-on-hover-off");
+  element.classList.remove("svg-on-hover-off");
+  element.classList.add("svg-on-hover");
+
+
+  let element3 =  document.querySelector(".flex-row-Auth6 .mtn-base");
+  element3.classList.remove("mtn-base");
+  element3.classList.add("mtn-on-hover");
+
+
+  let element4 =  document.querySelector(".flex-row-Auth6");
+  element4.classList.remove("flex-base");
+  element4.classList.add("flex-on-hover");
+}
+function resetStyleAuth6() {
+  let element2 = document.querySelector(".flex-row-Auth6 .svg-base-off");
+  element2.classList.remove("svg-base-off");
+  element2.classList.add("svg-base");
+
+  let element = document.querySelector(".flex-row-Auth6 .svg-on-hover");
+  element.classList.remove("svg-on-hover");
+  element.classList.add("svg-on-hover-off");
+
+  let element3 =  document.querySelector(".flex-row-Auth6 .mtn-on-hover");
+  element3.classList.remove("mtn-on-hover");
+  element3.classList.add("mtn-base");
+
+  let element4 =  document.querySelector(".flex-row-Auth6");
+  element4.classList.remove("flex-on-hover");
+  element4.classList.add("flex-base");
+}
 
 // function resetStyle() {
 //   let element = document.querySelector(".svg-Auth-on");
