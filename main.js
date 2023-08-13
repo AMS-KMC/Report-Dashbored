db1206f3a825fe930e7bc4c043e5ede1b773c59e
MahmoudSamnejad <m.samnejad92@gmail.com>
2023-08-13 14:49:58 +0330

    Open BTN

--- main.js
+++ main.js
@@ -529,15 +529,6 @@ function resetStyleAuth6() {
 // }
 
 //Samnejad's Section
-function openNav() {
-  document.getElementById("mySidebar").style.width = "300px";
-  document.getElementById("main").style.marginLeft = "300px";
-}
-
-function closeNav() {
-  document.getElementById("mySidebar").style.width = "0";
-  document.getElementById("main").style.marginLeft= "0";
-}
 
 
 
